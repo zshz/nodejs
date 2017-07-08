67640485ff575e98af07fa79cf673c0e8883b8ed
zshz <m18845128018@163.com>
2017-07-08 18:11:18 +0800

    一个简单的命令行文件浏览器，功能：允许用户读取和创建文件--重构--用fs进行文件操作

--- file-explorer/index.js
+++ file-explorer/index.js
@@ -14,9 +14,11 @@ fs.readdir(process.cwd(),function(err,files){
         return console.log('\033[31m Nofiles to show!\033[39m\n');
     }
     console.log('select which file to directory you want to see\n');
+    var stats=[];
     function file(i){
         var filename=files[i];
         fs.stat(__dirname+'/'+filename,function(err,stat){
+            stats[i]=stat;
             if(stat.isDirectory()){
                 console.log('   '+i+'   \033[36m'+filename+'/\033[39m');
             }else{
@@ -42,10 +44,25 @@ fs.readdir(process.cwd(),function(err,files){
         stdin.on('data',option);
     }
     function option(data){
-        if(!files[Number(data)]){
-            stdout.write(   '\033[31mEnter your choice:\033[39m');
-        }else{
-            stdin.pause();
+        var filename=files[Number(data)];
+        //if(!filename){
+        //    stdout.write(   '\033[31mEnter your choice:\033[39m');
+        //}
+        if(stats[Number(data)].isDirectory()) {
+            fs.readdir(__dirname+'/'+filename,function(err,files){
+                console.log('');
+                console.log(' 1   ('+files.length+'files)');
+                files.forEach(function(file){
+                    console.log('     -      '+file)
+                });
+                console.log('');
+            });
+        }else {
+            //stdin.pause();
+            fs.readFile(__dirname+'/'+filename,'utf8',function(err,data){
+                console.log('');
+                console.log('\033[90m'+data.replace(/ (.*)/g,'     $1')+'\033[39m');
+            });
         }
     }
     file(0);
